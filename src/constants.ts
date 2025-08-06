d9d43d619f1d088e839d969c90becf7d68fdbd1a
waywardphilosopher <will@willbuckingham.com>
2025-08-06 13:37:04 +0800

    Change social icons

--- src/constants.ts
+++ src/constants.ts
@@ -18,26 +18,14 @@ interface Social {
 
 export const SOCIALS: Social[] = [
   {
-    name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on GitHub`,
-    icon: IconGitHub,
-  },
-  {
-    name: "X",
-    href: "https://x.com/username",
-    linkTitle: `${SITE.title} on X`,
-    icon: IconBrandX,
-  },
-  {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "https://www.linkedin.com/in/willbuckingham/",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:will.buckingham@parami.edu.mm",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
