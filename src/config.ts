4c21b58af8f22e89d469880deb9f218f35787e76
waywardphilosopher <77926508+waywardphilosopher@users.noreply.github.com>
2025-08-12 08:52:26 +0100

    Small changes and titles

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ export const SITE = {
   title: "Dr. Will at Parami",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerIndex: 4,
-  postPerPage: 4,
+  postPerIndex: 8,
+  postPerPage: 8,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
